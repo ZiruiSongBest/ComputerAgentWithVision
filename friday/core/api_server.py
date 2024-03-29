fc3518d4c95b435e654b9458dcaad4eceb29885d
DylanLi-Hang <hang20145@gmail.com>
2024-03-29 11:42:34 +0000

    fix tools server

--- friday/core/api_server.py
+++ friday/core/api_server.py
@@ -7,19 +7,22 @@ from friday.core.server_config import ConfigManager
 app = FastAPI()
 
 # Import your services
-from friday.api.python.interpreter import router as python_router
 from friday.api.arxiv.arxiv import router as arxiv_router
+from friday.api.audio2text.audio2text_service import router as audio2text_router
 from friday.api.bing.bing_service import router as bing_router
 from friday.api.calculator.calculator import router as calculator_router
 from friday.api.chemical.chemical import router as chemical_router
-from friday.api.ppt.ppt import router as ppt_router
-from friday.api.shell.shell import router as shell_router
 from friday.api.database.database import router as db_router
-from friday.api.wolfram_alpha.wolfram_alpha import router as wa_router
-from friday.api.weather.weather import router as weather_router 
-from friday.api.google_calendar.calendar_service import router as calendar_router
 from friday.api.gmail.gmail import router as gmail_router
+from friday.api.google_calendar.calendar_service import router as calendar_router
+from friday.api.image_caption.image_caption_service import router as image_caption_router
 from friday.api.markdown.markdown_service import router as markdown_router
+from friday.api.ppt.ppt import router as ppt_router
+from friday.api.python.interpreter import router as python_router
+from friday.api.shell.shell import router as shell_router
+from friday.api.translate.translate import router as translate_router
+from friday.api.weather.weather import router as weather_router 
+from friday.api.wolfram_alpha.wolfram_alpha import router as wa_router
 
 from starlette.middleware.base import BaseHTTPMiddleware
 from starlette.requests import Request
@@ -54,12 +57,14 @@ services = {
     "weather": weather_router,
     "calendar": calendar_router,
     "gmail": gmail_router,
-    "markdown": markdown_router
-
+    "markdown": markdown_router,
+    "audio2text": audio2text_router,  # Added audio2text service
+    "image_caption": image_caption_router,  # Added image caption service
+    "translate": translate_router  # Added translate service
 }
 
 server_list = ["python_executor", "calculator","arxiv","bing","shell","ppt",
-               "database","wolframalpha","weather","calendar","gmail","markdown"]
+               "database","wolframalpha","weather","calendar","gmail","markdown", "audio2text", "image_caption", "translate"]
 
 # Include only the routers for the services listed in server_list
 for service in server_list:
@@ -72,4 +77,4 @@ for service in server_list:
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host=socket.gethostname(), port=8998)
+    uvicorn.run(app, host='localhost', port=8998)
